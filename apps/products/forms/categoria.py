f82fd028121cef4d51125ae4df28cd54305b26c6
Anthony Terán <aterana3@unemi.edu.ec>
2024-06-12 15:09:21 -0500

    Cambios de modelos y estructura

--- apps/products/forms/categoria.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from apps.products.models import Categoria
-
-class CategoriaForm(ModelForm):
-    class Meta:
-        model = Categoria
-        fields = '_all_'
-        exclude = ['created_at', 'created_by', 'updated_at', 'update_by']
