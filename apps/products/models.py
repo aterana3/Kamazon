1592897776f5a79530c02b7b0a1cf969d3cf5293
Anthony Terán <aterana3@unemi.edu.ec>
2024-06-12 15:09:21 -0500

    Cambios de modelos y estructura

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from apps.core.models import ModelBase
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
-class Category(models.Model):
+class Category(ModelBase):
     name = models.CharField(verbose_name='Name', max_length=50, unique=True)
     description = models.TextField(verbose_name='Description')
 
@@ -13,12 +17,13 @@ class Category(models.Model):
         verbose_name_plural = "Category's"
         ordering = ['-name']
 
-class Product(models.Model):
+class Product(ModelBase):
     name = models.CharField(verbose_name='Name', max_length=50, unique=True)
     description = models.TextField(verbose_name="Description")
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     stock = models.PositiveIntegerField(verbose_name='Stock', default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
