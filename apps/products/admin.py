3d62a7aa8b734ef49b761d71669a8a6a64dae822
Anthony Terán <aterana3@unemi.edu.ec>
2024-06-12 16:31:30 -0500

    Avance del proyecto

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
-from apps.products.models import Producto, Categoria
-admin.site.register(Producto)
-admin.site.register(Categoria)
+# Register your models here.
--- kamazon/urls.py
+++ kamazon/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path('', home.HomeView.as_view(), name='home'),
     path('auth/', include('apps.authentication.urls')),
     path('settings/', include('apps.settings.urls')),
-    path('products/', include('apps.products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
