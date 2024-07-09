777a18d098d3d1ecb48ad9ec708b11a27006a567
Anthony Terán <aterana3@unemi.edu.ec>
2024-07-09 02:52:16 -0500

    Mejoras en el diseño

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -4,7 +4,7 @@ from .views import products
 app_name = 'products'
 
 urlpatterns = [
-    path('list', products.ProductListView.as_view(), name='products'),
+    path('', products.ProductListView.as_view(), name='products_list'),
     path('<int:pk>/', products.ProductDetailView.as_view(), name='product_detail'),
     path('fetch/<int:pk>/', products.ProductDetailFetchView.as_view(), name='product_fetch'),
 ]
--- apps/products/views/products.py
+++ apps/products/views/products.py
@@ -2,6 +2,8 @@ from django.views.generic import DetailView, ListView
 from apps.products.models import Product, Category
 from django.http import JsonResponse
 from django.views import View
+from django.shortcuts import get_object_or_404
+
 
 class ProductDetailView(DetailView):
     model = Product
@@ -18,9 +20,18 @@ class ProductListView(ListView):
     template_name = 'product/list/page.html'
     context_object_name = 'products'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.request.GET.get('category')
+        if category_id:
+            category = get_object_or_404(Category, id=category_id)
+            queryset = queryset.filter(categories=category)
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
+        context['selected_category'] = self.request.GET.get('category')
         return context
 
 
